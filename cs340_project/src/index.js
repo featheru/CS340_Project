dd1a3dc9ee865c3e2d052870d9d1c70c4393f58a
Jonathon Shea <sheajonathon0@gmail.com>
2022-02-05 22:49:41 -0500

    Started routes and table templates

--- cs340_project/src/index.js
+++ cs340_project/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import AptOwners from './pages/AptOwners';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +22,14 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="AptOwners" element={<AptOwners />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
---
+++ cs340_project/src/pages/AptOwners.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { MdDeleteForever, MdEdit } from "react-icons/md";
+
+function AptOwners() {
+    return(
+        <table id="AptOwners">
+        <thead>
+            <tr>
+                <th>ownerID</th>
+                <th>firstName</th>
+                <th>lastName</th>
+                <th>ssn</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>0</td>
+            <td>Testy</td>
+            <td>Testerson</td>
+            <td>123-456-7890</td>
+            <td><MdEdit/></td>
+            <td><MdDeleteForever/></td>
+        </tr>
+        </tbody>
+    </table>
+
+
+
+
+    )
+}
+
+
+export default AptOwners;
