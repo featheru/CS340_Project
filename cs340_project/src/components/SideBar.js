c2b4aeacfb28ab13f29de003a2e4b39665d255ca
featheru <featheru@oregonstate.edu>
2022-03-14 13:23:13 -0400

    Updated ScrollBar

--- cs340_project/src/components/SideBar.js
+++ cs340_project/src/components/SideBar.js
@@ -1,23 +1,21 @@
-// sidebar.js
-
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import beaver from './WinkyBeaverJPG.jpg';
 
 export default props => {
   return (
-    <Menu>
+    <Menu overflow = "hidden">
       <div>
           <h1 id = "menu-item">Beaver Development Database Tables</h1>
           <img src = {beaver} width ="100" height = "100" alt ="Beaver Pic--TBD"></img>
       </div>
-      <a className="menu-item">Beaver Development User Database Tables</a>
-      <a className="menu-item">Project Step 6 Draft</a>
-      <a className="menu-item">CS 340: Intro to Databases</a>
-      <a className="menu-item">Group 93</a>
-      <a className="menu-item">Russell Feathers</a>
-      <a className="menu-item">Jonathon Shea</a>
-      <a className="menu-item">Due Date: 03/10/22</a>
+      <a>Final Project</a>
+      <a>CS 340: Intro to Databases</a>
+      <a>Group 93</a>
+      <a>Russell Feathers</a>
+      <a>Jonathon Shea</a>
+      <a>Due Date: 03/17/22</a>
+      <></>
     </Menu>
   );
 };
