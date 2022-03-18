14253df77fdfe21855239589593eb1b4e243cf81
Jonathon Shea <sheajonathon0@gmail.com>
2022-03-17 20:20:54 -0400

    Fixed deployment address.

--- cs340_project/src/ServerConstant.js
+++ cs340_project/src/ServerConstant.js
@@ -1,4 +1,4 @@
-export const AddressDeploy = 'http://flip2.engr.oregonstate.edu:5521';
+export const AddressDeploy = 'http://flip2.engr.oregonstate.edu:6363';
 export const AddressLocal = 'http://localhost:6363';
 export const AddressInUse = AddressDeploy;
 
