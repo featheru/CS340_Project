7a6ac15921d714fbc6ae06874f2dd10dd923397a
Jonathon Shea <sheajonathon0@gmail.com>
2022-03-17 20:12:03 -0400

    Changed server to deployment address.

--- cs340_project/src/ServerConstant.js
+++ cs340_project/src/ServerConstant.js
@@ -1,5 +1,5 @@
-export const AddressDeploy = 'http://flip2.engr.oregonstate.edu:6363';
+export const AddressDeploy = 'http://flip2.engr.oregonstate.edu:5521';
 export const AddressLocal = 'http://localhost:6363';
-export const AddressInUse = AddressLocal;
+export const AddressInUse = AddressDeploy;
 
 
